c2f010892ec5096b4535b3655b5f759622167fce
Rajeev S <raghu2raj@gmail.com>
2021-04-29 16:31:12 +0530

    add fib logic
    finding fib numbers using recursion

--- fibonacci_using_recursion/src/main.rs
+++ fibonacci_using_recursion/src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Fibonacci Generator!");
+    println!("use `q` to Quit");
+
+    loop {
+        let mut input = String::new();
+
+        println!("Enter a value:");
+        io::stdin().read_line(&mut input).expect("failed to read line");
+
+        if input.trim().to_lowercase() == "q" {
+            break;
+        }
+
+        // explicitly mention `u32` for correct parsing
+        let input: u32 = input.trim().parse().expect("parse failed");
+
+        println!("Fibonacci number is {}", fibonacci(input));
+
+    }
 }
+
+fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(n -1 ) + fibonacci( n-2),
+    }
+}
+
