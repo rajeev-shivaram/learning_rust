da4dfae1af303432b74eaa3931e851ca521593b9
Rajeev S <raghu2raj@gmail.com>
2021-05-04 22:06:46 +0530

    add new

---
+++ mean_median_mode/src/main.rs
@@ -0,0 +1,10 @@
+/*
+Given a list of integers, use a vector and return the mean (the average value),
+median (when sorted, the value in the middle position), and mode (the value that occurs most often;
+a hash map will be helpful here) of the list.
+ */
+
+
+fn main() {
+    println!("Hello, world!");
+}
